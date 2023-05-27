b952c9626bc3b3bdede3bb5b8c822a74c43d4dcb
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-26 21:53:14 -0600

    fixed the setup, thank God

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from pyodide.http import pyfetch
+import requests
 
-path= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
+path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
 
-async def download(url, filename):
-    response = await pyfetch(url)
-    if response.status == 200:
+def download(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
         with open(filename, "wb") as f:
-            f.write(await response.bytes())
+            f.write(response.content)
 
 download(path, "FuelConsumption.csv")
