b112eeaa7f5e9eb1fb8843b1e10b2e4726146968
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-26 21:42:42 -0600

    can visualize the data now, thank God

---
+++ setup.py
@@ -0,0 +1,11 @@
+from pyodide.http import pyfetch
+
+path= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
+
+async def download(url, filename):
+    response = await pyfetch(url)
+    if response.status == 200:
+        with open(filename, "wb") as f:
+            f.write(await response.bytes())
+
+download(path, "FuelConsumption.csv")
