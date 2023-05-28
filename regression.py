1169961016b1d6667a456ec0b7ff9d48759b3cc3
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-27 18:32:26 -0600

    creating a test set as well as creating a regression fit and getting coeffiencets, thank God

--- regression.py
+++ regression.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
 import numpy as np
+from sklearn import linear_model
+
+
 
 df = pd.read_csv("FuelConsumption.csv")
 
@@ -14,21 +17,39 @@ df.describe()
 cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 cdf.head(9)
 
-viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
-viz.hist()
-plt.show()
+# viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+# viz.hist()
+# plt.show()
 
-plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
-plt.xlabel("FUELCONSUMPTION_COMB")
-plt.ylabel("Emission")
-plt.show()
+# plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+# plt.xlabel("FUELCONSUMPTION_COMB")
+# plt.ylabel("Emission")
+# plt.show()
 
-plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+# plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+# plt.xlabel("Engine size")
+# plt.ylabel("Emission")
+# plt.show()
+
+# plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color='blue')
+# plt.xlabel("Cylinder")
+# plt.ylabel("Emission")
+# plt.show
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
 
-plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color='blue')
-plt.xlabel("Cylinder")
-plt.ylabel("Emission")
-plt.show
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
