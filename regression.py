b39c61ea3eca49a4f4f8c3a7559d6f144910c63d
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-27 18:46:40 -0600

    getting the errors, thank God

--- regression.py
+++ regression.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 from sklearn import linear_model
-
+from sklearn.metrics import r2_score
 
 
 df = pd.read_csv("FuelConsumption.csv")
@@ -53,3 +53,18 @@ regr.fit(train_x, train_y)
 # The coefficients
 print ('Coefficients: ', regr.coef_)
 print ('Intercept: ',regr.intercept_)
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y , test_y_) )
