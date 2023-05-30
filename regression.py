ab22cc22a2fc7971fe193c239014af1e8a6778d1
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-29 23:22:55 -0600

    working on lab 2, thank God

--- regression.py
+++ regression.py
@@ -21,65 +21,18 @@ cdf.head(9)
 # viz.hist()
 # plt.show()
 
-# plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
-# plt.xlabel("FUELCONSUMPTION_COMB")
-# plt.ylabel("Emission")
-# plt.show()
-
-# plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
-# plt.xlabel("Engine size")
-# plt.ylabel("Emission")
-# plt.show()
-
-# plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color='blue')
-# plt.xlabel("Cylinder")
-# plt.ylabel("Emission")
-# plt.show
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
 
 msk = np.random.rand(len(df)) < 0.8
 train = cdf[msk]
 test = cdf[~msk]
 
-
-# plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
-# plt.xlabel("Engine size")
-# plt.ylabel("Emission")
-# plt.show()
-
 regr = linear_model.LinearRegression()
-train_x = np.asanyarray(train[['ENGINESIZE']])
-train_y = np.asanyarray(train[['CO2EMISSIONS']])
-regr.fit(train_x, train_y)
+x = np.asanyarray(train[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit (x, y)
 # The coefficients
 print ('Coefficients: ', regr.coef_)
-print ('Intercept: ',regr.intercept_)
-
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
-plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
-plt.xlabel("Engine size")
-plt.ylabel("Emission")
-plt.show()
-
-
-test_x = np.asanyarray(test[['ENGINESIZE']])
-test_y = np.asanyarray(test[['CO2EMISSIONS']])
-test_y_ = regr.predict(test_x)
-
-print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
-print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
-print("R2-score: %.2f" % r2_score(test_y , test_y_) )
-
-
-train_x = np.asanyarray(train[["FUELCONSUMPTION_COMB"]])
-test_x = np.asanyarray(test[["FUELCONSUMPTION_COMB"]])
-
-regr = linear_model.LinearRegression()
-regr.fit(train_x, train_y)
-
-# The coefficients
-print('Coefficients:', regr.coef_)
-print('Intercept:', regr.intercept_)
-
-test_y_ = regr.predict(test_x)
-
-print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
