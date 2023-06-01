3d9caf78a3f05bc0a1e965cbcd9fcc4f16044c37
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-01 13:59:38 -0600

    started the polynomial lab, thank God

---
+++ polynomial.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+y = 2 * x + 3
+
+y_noise = np.random.normal(size=x.size)
+
+y_data = y + y_noise
+
+plt.scatter(x, y_data, color='blue')
+plt.plot(x, y, color='red')
+
+plt.xlabel("Independet Variables")
+plt.ylabel("Dependet Variables")
+
+plt.show()
+
