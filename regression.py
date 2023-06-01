9b7ddaf1110a0b13f112ce63332c36afec5953ff
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-31 23:12:24 -0600

    non-linear regression lab done, comments added, thank God

--- regression.py
+++ regression.py
@@ -6,6 +6,7 @@ from sklearn import linear_model
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import PolynomialFeatures
 
+# data prep
 
 df = pd.read_csv("FuelConsumption.csv")
 
@@ -39,36 +40,40 @@ train_y = np.asanyarray(train[['CO2EMISSIONS']])
 test_x = np.asanyarray(test[['ENGINESIZE']])
 test_y = np.asanyarray(test[['CO2EMISSIONS']])
 
+# non-linear regression with degree 2
 
-# poly = PolynomialFeatures(degree=2)
-# train_x_poly = poly.fit_transform(train_x)
-# print(train_x_poly)
+poly = PolynomialFeatures(degree=2)
+train_x_poly = poly.fit_transform(train_x)
+print(train_x_poly)
 
-# train_y_ = regr.fit(train_x_poly, train_y)
+train_y_ = regr.fit(train_x_poly, train_y)
 
-# print(regr.coef_)
-# print(regr.intercept_)
+print(regr.coef_)
+print(regr.intercept_)
 
-# plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
 
-# XX = np.arange(0.0, 10.0, 0.1)
+XX = np.arange(0.0, 10.0, 0.1)
 
-# yy = regr.intercept_[0] + regr.coef_[0][1] *  XX + regr.coef_[0][2] * (XX ** 2)
+yy = regr.intercept_[0] + regr.coef_[0][1] *  XX + regr.coef_[0][2] * (XX ** 2)
 
-# plt.plot(XX, yy, '-r')
+plt.plot(XX, yy, '-r')
 
-# plt.xlabel("ENGINESIZE")
-# plt.ylabel("CO2EMISSIONS")
+plt.xlabel("ENGINESIZE")
+plt.ylabel("CO2EMISSIONS")
 
-# plt.show()
+plt.show()
 
-# test_x_poly = poly.transform(test_x)
-# test_y_ = regr.predict(test_x_poly)
+# non-linear regression testing with degree 2
 
-# print("Absolute Mean Error: %.2f" % np.mean(np.abs(test_y_ - test_y)))
-# print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
-# print("R2 score: %.2f" % r2_score(test_y, test_y_))
+test_x_poly = poly.transform(test_x)
+test_y_ = regr.predict(test_x_poly)
 
+print("Absolute Mean Error: %.2f" % np.mean(np.abs(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2 score: %.2f" % r2_score(test_y, test_y_))
+
+# non-linear regression with degree 3
 
 poly = PolynomialFeatures(degree=3)
 train_x_poly = poly.fit_transform(train_x)
@@ -90,6 +95,8 @@ plt.xlabel("ENGINESIZE")
 plt.ylabel("CO2EMISSIONS")
 
 plt.show()
+
+# non-linear regression with degree 3
 
 test_x_poly = poly.transform(test_x)
 test_y_ = regr.predict(test_x_poly)
