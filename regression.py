19da8fa581aec374530615933b99900e4eaccd35
saji2000 <sajaddaneshmand79@gmail.com>
2023-05-31 22:47:55 -0600

    non-linear regression trying with degree=3, thank God

--- regression.py
+++ regression.py
@@ -40,20 +40,49 @@ test_x = np.asanyarray(test[['ENGINESIZE']])
 test_y = np.asanyarray(test[['CO2EMISSIONS']])
 
 
-poly = PolynomialFeatures(degree=2)
+# poly = PolynomialFeatures(degree=2)
+# train_x_poly = poly.fit_transform(train_x)
+# print(train_x_poly)
+
+# train_y_ = regr.fit(train_x_poly, train_y)
+
+# print(regr.coef_)
+# print(regr.intercept_)
+
+# plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+
+# XX = np.arange(0.0, 10.0, 0.1)
+
+# yy = regr.intercept_[0] + regr.coef_[0][1] *  XX + regr.coef_[0][2] * (XX ** 2)
+
+# plt.plot(XX, yy, '-r')
+
+# plt.xlabel("ENGINESIZE")
+# plt.ylabel("CO2EMISSIONS")
+
+# plt.show()
+
+# test_x_poly = poly.transform(test_x)
+# test_y_ = regr.predict(test_x_poly)
+
+# print("Absolute Mean Error: %.2f" % np.mean(np.abs(test_y_ - test_y)))
+# print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+# print("R2 score: %.2f" % r2_score(test_y, test_y_))
+
+
+poly = PolynomialFeatures(degree=3)
 train_x_poly = poly.fit_transform(train_x)
-print(train_x_poly)
 
 train_y_ = regr.fit(train_x_poly, train_y)
 
-print(regr.coef_)
 print(regr.intercept_)
+print(regr.coef_)
 
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color='black')
 
 XX = np.arange(0.0, 10.0, 0.1)
 
-yy = regr.intercept_[0] + regr.coef_[0][1] *  XX + regr.coef_[0][2] * (XX ** 2)
+yy = regr.intercept_[0] + regr.coef_[0][1] * XX + regr.coef_[0][2] * (XX ** 2) + regr.coef_[0][3] * (XX ** 3)
 
 plt.plot(XX, yy, '-r')
 
@@ -62,9 +91,4 @@ plt.ylabel("CO2EMISSIONS")
 
 plt.show()
 
-test_x_poly = poly.transform(test_x)
-test_y_ = regr.predict(test_x_poly)
 
-print("Absolute Mean Error: %.2f" % np.mean(np.abs(test_y_ - test_y)))
-print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
-print("R2 score: %.2f" % r2_score(test_y, test_y_))
