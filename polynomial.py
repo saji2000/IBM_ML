eac3692fd305cc174a331031bae9e6a4456045c5
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-02 00:05:46 -0600

    sigmoid graph, thank God

--- polynomial.py
+++ polynomial.py
@@ -6,14 +6,20 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("china_gdp.csv")
 df.head(10)
 
-plt.figure(figsize=(8, 5))
+# plt.figure(figsize=(8, 5))
 
-x_data, y_data = (df['Year'].values, df['Value'].values)
+# x_data, y_data = (df['Year'].values, df['Value'].values)
 
-plt.scatter(x_data, y_data, color='blue')
-plt.xlabel('Year')
-plt.ylabel('Value')
+# plt.scatter(x_data, y_data, color='blue')
+# plt.xlabel('Year')
+# plt.ylabel('Value')
+# plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = 1.0 / (1.0 + np.exp(-X))
+
+plt.plot(X,Y) 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
 plt.show()
-
-
 
