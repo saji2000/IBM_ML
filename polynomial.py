75c05811493689d6ed16477d7c690617b0287a72
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-02 20:16:21 -0600

    sigmoid graph for china gdp, thank God

--- polynomial.py
+++ polynomial.py
@@ -1,24 +1,26 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
 
 
 df = pd.read_csv("china_gdp.csv")
 df.head(10)
 
-# plt.figure(figsize=(8, 5))
+plt.figure(figsize=(8, 5))
 
-# x_data, y_data = (df['Year'].values, df['Value'].values)
+x_data, y_data = (df['Year'].values, df['Value'].values)
 
-# plt.scatter(x_data, y_data, color='blue')
-# plt.xlabel('Year')
-# plt.ylabel('Value')
-# plt.show()
+plt.scatter(x_data, y_data, color='blue')
+plt.xlabel('Year')
+plt.ylabel('Value')
+plt.show()
+
 def sigmoid(x, Beta_1, Beta_2):
      y = 1 / (1 + np.exp(-Beta_1*(x-Beta_2)))
      return y
-
-x_data = np.arange(-5.0, 5.0, 0.1)
 
 beta_1 = 0.10
 beta_2 = 1990.0
@@ -28,7 +30,7 @@ Y_pred = sigmoid(x_data, beta_1 , beta_2)
 
 #plot initial prediction against datapoints
 plt.plot(x_data, Y_pred*15000000000000.)
-# plt.plot(x_data, y_data, 'ro')
+plt.plot(x_data, y_data, 'ro')
 
 # plt.plot(x_data,Y_pred) 
 plt.ylabel('Dependent Variable')
