4e7fa4dcf87d1f9337546e1b19d2efcd31b3f6ff
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-04 16:25:19 -0600

    allocating train/test split, thank God

--- knn.py
+++ knn.py
@@ -3,6 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('teleCust1000t.csv')
-df.head()
+
+x = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+x[0:5]
+
+y = df['custcat'].values
+y[0:5]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=4)
+
+print("Train set: ", x_train.shape, y_train.shape)
+
+print("Test set: ", x_test.shape, y_test.shape)
