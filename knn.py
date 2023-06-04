71043fd5e3e7fd6101fb92131231b35c2b575dab
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-04 15:35:57 -0600

    knn started, thank God

---
+++ knn.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+df = pd.read_csv('teleCust1000t.csv')
+df.head()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import requests
 
-path='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/china_gdp.csv'
+path="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv"
 
 def download(url, filename):
     response = requests.get(url)
