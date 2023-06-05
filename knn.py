4c5c5489f8f3de17a7ab32135a650355ab9ba56d
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-05 16:46:42 -0600

    Comparing accuracies depending on different Ks and choosing the most accurate, thank God

--- knn.py
+++ knn.py
@@ -29,16 +29,41 @@ x_test_norm[0:5]
 
 k = 4
 
-neigh = KNeighborsClassifier(n_neighbors = k).fit(x_train_norm, y_train)
+# neigh = KNeighborsClassifier(n_neighbors = k).fit(x_train_norm, y_train)
 
-print("Train Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(x_train_norm)))
-print("Test Accuracy: ", metrics.accuracy_score(y_test, neigh.predict(x_test_norm)))
+# print("Train Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(x_train_norm)))
+# print("Test Accuracy: ", metrics.accuracy_score(y_test, neigh.predict(x_test_norm)))
 
-k = 6
+# k = 6
 
-neigh = KNeighborsClassifier(n_neighbors = k).fit(x_train_norm, y_train)
+# neigh = KNeighborsClassifier(n_neighbors = k).fit(x_train_norm, y_train)
 
-print("Train Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(x_train_norm)))
-print("Test Accuracy: ", metrics.accuracy_score(y_test, neigh.predict(x_test_norm)))
+# print("Train Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(x_train_norm)))
+# print("Test Accuracy: ", metrics.accuracy_score(y_test, neigh.predict(x_test_norm)))
+
+Ks = 10
+
+mean_acc = np.zeros(Ks-1)
+std_acc = np.zeros(Ks-1)
+
+for i in range(1, Ks):
+
+    neigh = KNeighborsClassifier(n_neighbors = i).fit(x_train_norm, y_train)
+
+    y_hat = neigh.predict(x_test_norm)
+
+    mean_acc[i-1] = metrics.accuracy_score(y_test, y_hat)
+
+    std_acc[i-1]=np.std(y_hat==y_test)/np.sqrt(y_hat.shape[0])
 
 
+plt.plot(range(1,Ks),mean_acc,'g')
+plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
+plt.fill_between(range(1,Ks),mean_acc - 3 * std_acc,mean_acc + 3 * std_acc, alpha=0.10,color="green")
+plt.legend(('Accuracy ', '+/- 1xstd','+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Neighbors (K)')
+plt.tight_layout()
+plt.show()
+
+print( "The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax()+1) 
