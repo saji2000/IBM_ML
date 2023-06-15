a0d127db2ce12f00b0b6341251f4c588a1f1f780
saji2000 <sajaddaneshmand79@gmail.com>
2023-06-15 13:45:31 -0600

    optional lab finished

--- module_1/regression.py
+++ module_1/regression.py
@@ -17,3 +17,56 @@ print(f"Number of training examples is: {m}")
 # m is the number of training examples
 m = len(x_train)
 print(f"Number of training examples is: {m}")
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r')
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.show()
+
+def compute_model_output(x, w, b):
+    """
+    Computes the prediction of a linear model
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      w,b (scalar)    : model parameters  
+    Returns
+      y (ndarray (m,)): target values
+    """
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+        
+    return f_wb
+
+w = 100
+b = 100
+
+tmp_f_wb = compute_model_output(x_train, w, b,)
+
+# Plot our model prediction
+plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
+
+w = 200                         
+b = 100    
+x_i = 1.2
+cost_1200sqft = w * x_i + b    
+
+print(f"${cost_1200sqft:.0f} thousand dollars")
