2fd1d085764ea2efb255081a4d02eae2dbe03fe6
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-04 21:32:53 -0600

    compute regression cost

--- module_1/regression.py
+++ module_1/regression.py
@@ -38,3 +38,39 @@ def compute_cost(x, y, w, b):
     ### END CODE HERE ### 
 
     return total_cost
+
+# UNQ_C2
+# GRADED FUNCTION: compute_gradient
+def compute_gradient(x, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      x (ndarray): Shape (m,) Input to the model (Population of cities) 
+      y (ndarray): Shape (m,) Label (Actual profits for the cities)
+      w, b (scalar): Parameters of the model  
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    
+    # Number of training examples
+    m = x.shape[0]
+    
+    # You need to return the following variables correctly
+    dj_dw = 0
+    dj_db = 0
+    
+    ### START CODE HERE ###
+    
+    cost = np.dot(w, x) + b
+    
+    for i in range(0, m):
+        dj_dw += (cost[i] - y[i]) * x[i]
+        dj_db += (cost[i] - y[i])
+        
+    dj_dw = dj_dw/m
+    dj_db = dj_db/m
+    
+    ### END CODE HERE ### 
+        
+    return dj_dw, dj_db
